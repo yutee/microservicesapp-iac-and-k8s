f74f2256e23d96c01ac9e4e2bfeb87c8b5877ad8
yutee <yuteenightxi@gmail.com>
2024-08-15 14:58:48 +0100

    should workk

--- terraform/variable.tf
+++ terraform/variable.tf
@@ -6,3 +6,9 @@
 # variable "password" {
 #   description = "The Azure AD Application Password"
 # }
+
+# github actions
+variable "client_id" {}
+variable "client_secret" {}
+variable "tenant_id" {}
+variable "subscription_id" {}
